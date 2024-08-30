6429ce92bcdb1eb7bd3dd79183f091fc0b617b73
Prerit Jain <121391267+prerit85jain@users.noreply.github.com>
2024-08-30 15:26:41 +0530

    Add files via upload

---
+++ testing.py
@@ -0,0 +1,39 @@
+import pickle
+import numpy as np
+from numpy.linalg import norm
+import tensorflow
+from tensorflow.keras.preprocessing import image
+from tensorflow.keras.layers import GlobalMaxPooling2D
+from keras.applications.vgg16 import VGG16,  preprocess_input
+from sklearn.neighbors import NearestNeighbors
+import cv2
+
+feature_list = np.array(pickle.load(open('embedding.pkl', 'rb')))
+filenames = pickle.load(open('filenames.pkl', 'rb'))
+
+model = VGG16(weights='imagenet',include_top=False,input_shape=(224,224,3))
+model.trainable = False
+
+model = tensorflow.keras.Sequential([
+    model,
+    GlobalMaxPooling2D()
+])
+
+img = image.load_img('sample/9867.jpg', target_size=(224, 224))
+img_array = image.img_to_array(img)
+expanded_img_array = np.expand_dims(img_array, axis=0)
+preprocess_img = preprocess_input(expanded_img_array)
+result = model.predict(preprocess_img).flatten()
+normalized_result = result/norm(result)
+
+# calculate the distance b/w feature_list & normalized_result
+neighbors = NearestNeighbors(n_neighbors=6, algorithm='brute', metric='euclidean')
+neighbors.fit(feature_list)
+
+distances, indices = neighbors.kneighbors([normalized_result])
+print(indices)
+
+for file in indices[0][1:6]:
+    temp_img = cv2.imread(filenames[file])
+    cv2.imshow('output', cv2.resize(temp_img, (512, 512)))
+    cv2.waitKey(0)
